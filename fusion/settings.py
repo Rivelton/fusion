6210c1d50010e22ea434f7cb89b96e079424c79b
craun <rivelton.lima.12@gmail.com>
2021-08-29 19:25:52 -0300

    teste5

--- fusion/settings.py
+++ fusion/settings.py
@@ -129,7 +129,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-MEDIA_URL = 'media/'
+MEDIA_URL = '/media/'
 STATIC_ROOT = Path(BASE_DIR, 'staticfiles')
 MEDIA_ROOT = Path(BASE_DIR, 'media')
 
